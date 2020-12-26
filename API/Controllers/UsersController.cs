38236f9aae27aaac9814f39a70e1e3b981e66315
Adam Tappy <no_email_4_u@no.ty>
2020-12-26 14:49:36 +0100

    now with more async

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -18,14 +20,14 @@ namespace API.Controllers
 
         // api/users
         [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetUsers(){
-            return _context.Users.ToList();
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+            return await _context.Users.ToListAsync();
         }
 
         // api/users/1
         [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUser(int id){
-            return _context.Users.Find(id);
+        public async Task<ActionResult<AppUser>> GetUser(int id){
+            return await _context.Users.FindAsync(id);
         }
     }
 }
